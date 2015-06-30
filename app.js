9776d34ecdc6ab7475fa6b260231910e0165c130
UHH <student10@JS-C02MXKJHG083.local>
2015-06-30 13:02:43 -1000

    all one alert

--- app.js
+++ app.js
@@ -16,31 +16,25 @@ function scissors(){
 function paper(){
 	var computerChoice = Math.floor(Math.random() * 3);
 	if (computerChoice === 0) {
-		alert("The computer chose scissors");
-		alert("You lose");
+		alert("The computer chose scissors, and you lose!");
 	}
 	else if(computerChoice === 1) {
-		alert("The computer chose rock");
-		alert("You win");
+		alert("The computer chose rock, and you win!");
 		}
 	else if(computerChoice === 2) {
-		alert("The computer chose paper");
-		alert("You tie");
+		alert("The computer chose paper, and you tie!");
 	}
 }
 //0 = scissors     1 = rock     2 = paper
 function rock(){
 	var computerChoice = Math.floor(Math.random() * 3);
 	if (computerChoice === 0) {
-		alert("The computer chose scissors");
-		alert("You win");
+		alert("The computer chose scissors, and you win!");
 	}
 	else if(computerChoice === 1) {
-		alert("The computer chose rock");
-		alert("You tie");
+		alert("The computer chose rock, and you tie!");
 		}
 	else if(computerChoice === 2) {
-		alert("The computer chose paper");
-		alert("You lose");
+		alert("The computer chose paper, and you lose!");
 	}
 }
