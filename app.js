7ed81818827ace4a0a80c0ca8eb7a70eed780810
UHH <student10@JS-C02MXKJHG083.local>
2015-06-30 13:59:19 -1000

    took away alerts, now prints results prints score on side, ignores ties STYLED LIKE A BOSS

--- app.js
+++ app.js
@@ -4,36 +4,48 @@ var compScore = 0;
 function scissors(){
 	var computerChoice = Math.floor(Math.random() * 3);
 	if (computerChoice === 0) {
-		alert("The computer chose scissors, and it's a tie!");
+		document.getElementById("displayResult").innerHTML = "The computer chose scissors, and it's a tie!";
 	}
 	else if(computerChoice === 1) {
-		alert("The computer chose rock, and you lose!");
+		document.getElementById("displayResult").innerHTML = "The computer chose rock, and you lose!";
+		compScore++;
+		document.getElementById("disCompScore").innerHTML = compScore;
 		}
 	else if(computerChoice === 2) {
-		alert("The computer chose paper, and you win!");
+		document.getElementById("displayResult").innerHTML = "The computer chose paper, and you win!";
+		playerScore++;
+		document.getElementById("disPlayerScore").innerHTML = playerScore;
 	}
 }
 function paper(){
 	var computerChoice = Math.floor(Math.random() * 3);
 	if (computerChoice === 0) {
-		alert("The computer chose scissors, and you lose!");
+		document.getElementById("displayResult").innerHTML = "The computer chose scissors, and you lose!";
+		compScore++;
+		document.getElementById("disCompScore").innerHTML = compScore;
 	}
 	else if(computerChoice === 1) {
-		alert("The computer chose rock, and you win!");
+		document.getElementById("displayResult").innerHTML = "The computer chose rock, and you win!";
+		playerScore++;
+		document.getElementById("disPlayerScore").innerHTML = playerScore;
 		}
 	else if(computerChoice === 2) {
-		alert("The computer chose paper, and you tie!");
+		document.getElementById("displayResult").innerHTML = "The computer chose paper, and you tie!";
 	}
 }
 function rock(){
 	var computerChoice = Math.floor(Math.random() * 3);
 	if (computerChoice === 0) {
-		alert("The computer chose scissors, and you win!");
+		document.getElementById("displayResult").innerHTML = "The computer chose scissors, and you win!";
+		playerScore++;
+		document.getElementById("disPlayerScore").innerHTML = playerScore;
 	}
 	else if(computerChoice === 1) {
-		alert("The computer chose rock, and you tie!");
+		document.getElementById("displayResult").innerHTML = "The computer chose rock, and you tie!";
 		}
 	else if(computerChoice === 2) {
-		alert("The computer chose paper, and you lose!");
+		document.getElementById("displayResult").innerHTML = "The computer chose paper, and you lose!";
+		compScore++;
+		document.getElementById("disCompScore").innerHTML = compScore;
 	}
 }
